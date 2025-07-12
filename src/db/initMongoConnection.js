fc0e87fb56333ea70e479b412ca152f207f3feab
Nazar <nazarijankosuk@gmail.com>
2025-07-12 12:05:51 +0300

    7 hw changes

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -2,12 +2,17 @@ import mongoose from 'mongoose';
 import { getEnvVariable } from '../utilts/getEnvVariable.js';
 
 export async function initMongoConnection() {
-  const user = getEnvVariable('MONGODB_USER');
-  const pwd = getEnvVariable('MONGODB_PASSWORD');
-  const url = getEnvVariable('MONGODB_URL');
-  const db = getEnvVariable('MONGODB_DB');
+  try {
+    const user = getEnvVariable('MONGODB_USER');
+    const pwd = getEnvVariable('MONGODB_PASSWORD');
+    const url = getEnvVariable('MONGODB_URL');
+    const db = getEnvVariable('MONGODB_DB');
 
-  await mongoose.connect(
-    `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=ClusterStart`,
-  );
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=ClusterStart`,
+    );
+    console.log("DB succeffully loaded!");
+  } catch (error) {
+    console.log('Error:', error);
+  }
 }
--- src/utilts/getEnvVariable.js
+++ src/utilts/getEnvVariable.js
@@ -1,3 +1,5 @@
+import "dotenv/config";
+
 export function getEnvVariable(name) {
   const value = process.env[name];
 
