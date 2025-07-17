3f1cfc87330794fd38cc1e249ab7ed26e634672f
Nazar <nazarijankosuk@gmail.com>
2025-07-18 00:57:58 +0300

    3 hw changes

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,5 +1,5 @@
-import createHttpError from 'http-errors';
-
 export default function notFoundHandler(req, res, next) {
-    next(createHttpError(404, 'Route not found'));
+  res
+    .status(404)
+    .json({ status: 404, message: 'Route not found', data: res.data });
 }
